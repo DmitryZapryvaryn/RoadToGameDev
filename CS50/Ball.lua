505a719cf11751e1757e15667087eb6096d8155b
Dmitry <zapdmitry@gmail.com>
2018-09-10 01:35:29 +0300

    Add score logic and counter; add sounds; add serve logic

--- CS50/Ball.lua
+++ CS50/Ball.lua
@@ -6,8 +6,8 @@ function Ball:init(x, y, width, height)
     self.width = width
     self.height = height
 
-    self.dy = math.random(2) == 1 and 100 or -100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and 100 or -100
+    self.dy = math.random(-50, 50)
 end
 
 function Ball:collides(paddle)
